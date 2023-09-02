b7d571ad5375193965c70ff8b5a83efa93f1d7c5
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-03 00:46:28 +0530

    rename values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const values = [7, 8, 9, '/', 4, 5, 6, 'x', 1, 2, 3, '-', 0, '.', '=', '+']
-  const handleClick = (value) => {
-    console.log(`you clicked ${value}`);
+  const [values, setValues] = useState('')
+  const items = [7, 8, 9, '/', 4, 5, 6, 'x', 1, 2, 3, '-', 0, '.', '=', '+']
+  const handleClick = (e) => {
+
 
   }
 
@@ -15,12 +17,13 @@ function App() {
         <h1>Calculator</h1>
 
         <div className="main-input">
-          <input type="number" placeholder="0"></input>
+          <input
+            type="number" placeholder="0"></input>
 
         </div>
         <div className="all-btn">
-          {values.map((value, index) => (
-            <button key={index} onClick={() => handleClick(value)} className="btn">{value}</button>
+          {items.map((item, index) => (
+            <button key={index} onClick={() => handleClick(item)} className="btn">{item}</button>
           ))}
 
         </div>
