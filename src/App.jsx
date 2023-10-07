b72bc62393d0285f2a0fc0af31bb1bbdebc29b62
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-10-07 08:48:20 +0530

    style-resdesign the skeleton of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-
 import { useState } from 'react';
 import './App.css'
 import { items } from './constants';
@@ -30,26 +29,39 @@ function App() {
 
   return (
     <>
-      <div id="container">
-        <h1>Calculator</h1>
+      <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center p-2  rounded-lg shadow-2xl bg-black'>
+          <div className='w-[50vw] h-[50vh] flex flex-col shadow-xl'>
+            <h1 className='rounded-t-lg text-center bg-[#4B4B92] text-white py-2 font-mono'>Calculator</h1>
 
-        <div className="main-input">
-          <input
-            type="text"
-            placeholder="0"
-            readOnly
-            value={input}
-          />
-
+            <div className=" w-full h-20 bg-[#FFF3DA] ">
+              {result ? (
+                <p className='h-full bg-[#222E35] text-white text-right flex justify-end items-center text-2xl px-2 '>{result}</p>
+              ) : (
+                <input
+                  type="text"
+                  placeholder="0"
+                  readOnly
+                  value={input}
+                  className='w-full h-full text-white text-right text-2xl px-2 focus:outline-none bg-[#222E35]'
+                />
+              )}
+            </div>
+            <div className="grid grid-cols-4 grid-rows-4 gap-1 bg-white shadow-md h-full rounded-b-lg">
+              {items.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleClick(item)}
+                  className=' btn text-2xl text-white bg-[#655796] hover:bg-[#2d164e] rounded-lg m-1'
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="all-btn">
-          {items.map((item, index) => (
-            <button key={index} onClick={() => handleClick(item)} className="btn">{item}</button>
-          ))}
-
-        </div>
-        <p> Result: {result}</p>
       </div>
+
 
     </>
   )
